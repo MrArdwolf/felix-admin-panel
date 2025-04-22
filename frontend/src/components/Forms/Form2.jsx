38e97d88374298fee867f091b0fc8ce8f0cdd609
MrArdwolf <elias@folkestorp.se>
2025-04-22 14:32:29 +0200

    form 2 done

--- frontend/src/components/Forms/Form2.jsx
+++ frontend/src/components/Forms/Form2.jsx
@@ -7,37 +7,79 @@ export default function Form() {
     <form action="" className="Form1">
       <div className="inputs">
         <div className="input-row">
-          <label htmlFor="email">Email:</label>
-          <input type="text" id="email" />
-        </div>
-        <div className="input-row">
-          <label htmlFor="username">Username</label>
-          <input type="text" id="username" />
-        </div>
-        <div className="input-column">
-          <div className="input-row">
-            <label htmlFor="first-name">First Name:</label>
-            <input type="text" id="first-name" />
+          <label>Kan ni kolla/fixa</label>
+          <div className="front-back-row">
+            <span>Fram</span>
+            <span>Bak</span>
           </div>
-          <div className="input-row">
-            <label htmlFor="last-name">Last Name:</label>
-            <input type="text" id="last-name" />
+          <div className="checkbox-row">
+            <label htmlFor="punctured">Punktering</label>
+            <input type="checkbox" name="punctured" id="punctured-front" />
+            <input type="checkbox" name="punctured" id="punctured-back" />
+          </div>
+          <div className="checkbox-row">
+            <label htmlFor="punctured">Byte däck</label>
+            <input type="checkbox" name="punctured" id="punctured-front" />
+            <input type="checkbox" name="punctured" id="punctured-back" />
+          </div>
+          <div className="checkbox-row">
+            <label htmlFor="punctured">Byte växelvajer</label>
+            <input type="checkbox" name="punctured" id="punctured-front" />
+            <input type="checkbox" name="punctured" id="punctured-back" />
+          </div>
+          <div className="checkbox-row">
+            <label htmlFor="punctured">Byte bromsvajer</label>
+            <input type="checkbox" name="punctured" id="punctured-front" />
+            <input type="checkbox" name="punctured" id="punctured-back" />
+          </div>
+          <div className="checkbox-row">
+            <label htmlFor="punctured">Justera växel</label>
+            <input type="checkbox" name="punctured" id="punctured-front" />
+            <input type="checkbox" name="punctured" id="punctured-back" />
+          </div>
+          <div className="checkbox-row">
+            <label htmlFor="punctured">Justera broms</label>
+            <input type="checkbox" name="punctured" id="punctured-front" />
+            <input type="checkbox" name="punctured" id="punctured-back" />
+          </div>
+          <div className="checkbox-row">
+            <label htmlFor="punctured">Rikta hjul</label>
+            <input type="checkbox" name="punctured" id="punctured-front" />
+            <input type="checkbox" name="punctured" id="punctured-back" />
+          </div>
+          <div className="checkbox-row">
+            <label htmlFor="punctured">Byta eker</label>
+            <input type="checkbox" name="punctured" id="punctured-front" />
+            <input type="checkbox" name="punctured" id="punctured-back" />
           </div>
         </div>
+
+
         <div className="input-row">
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" />
-        </div>
-        <div className="input-row">
-          <label htmlFor="confirm-password">Confirm Password:</label>
-          <input type="password" id="confirm-password" />
-        </div>
-        <div className="input-row">
-          <label htmlFor="master-password">Master Password:</label>
-          <input type="password" id="master-password" />
+          <label htmlFor="also-do">Utför även</label>
+          <div className="checkbox-row">
+            <input type="checkbox" name="change-chain" id="change-chain" />
+            <label htmlFor="change-chain">Byte kedja</label>
+          </div>
+          <div className="checkbox-row">
+            <input type="checkbox" name="lubricate" id="lubricate" />
+            <label htmlFor="lubricate">Smörja</label>
+          </div>
+          <div className="checkbox-row">
+            <input type="checkbox" name="pump" id="pump" />
+            <label htmlFor="pump">Pumpa</label>
+          </div>
+          <div className="checkbox-row">
+            <input type="checkbox" name="wash" id="wash" />
+            <label htmlFor="wash">Tvätta</label>
+          </div>
+
+
         </div>
       </div>
-      <button type="submit">Sign Up</button>
+      <button>Bakåt</button>
+      <button type="submit">Nästa</button>
+      <button>Rensa formuläret</button>
     </form>
   )
 }
--- frontend/src/pages/FormPage/FormPage.jsx
+++ frontend/src/pages/FormPage/FormPage.jsx
@@ -1,11 +1,12 @@
 
 import "./FormPage.css"
 import Form from "../../components/Forms/Form1"
+import Form2 from "../../components/Forms/Form2"
 
 export default function FormPage() {
   return (
     <div className="FormPage">
-      <Form />
+      <Form2 />
     </div>
   )
 }
