2f4d73beea779e9c3ddcb9433a43c538102a2114
MrArdwolf <elias@folkestorp.se>
2025-05-21 10:02:50 +0200

    sms modal css done

--- frontend/src/components/SmsModal/SmsModal.jsx
+++ frontend/src/components/SmsModal/SmsModal.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './SmsModal.scss'
 
 
 export default function SmsModal(props) {
@@ -6,24 +7,26 @@ export default function SmsModal(props) {
   return (
     <div className="sms-modal">
       <div className="sms-modal-content">
+        <h3>Skicka SMS till {props.customer.name}</h3>
         <div className="price-sms">
           <h3>Skicka prisförslag SMS</h3>
-          <p>Skicka SMS till {props.customer.name}</p>
-          <button onClick={props.sendPriceSMS}>Skicka</button>
+          <button className='primary-button' onClick={props.sendPriceSMS}>Skicka</button>
         </div>
         <div className="done-sms">
           <h3>Skicka klar SMS</h3>
-          <p>Skicka SMS till {props.customer.name}</p>
-          <label htmlFor="lock">Lås:</label>
-          <select name="lock" id="lock" onChange={(e) => { setLock(e.target.value) }}>
-            <option value="375">Inget</option>
-            <option value="059">Gul</option>
-            <option value="024">Silver</option>
-            <option value="692">Svart</option>
-          </select>
-          <button onClick={() => {props.sendDoneSMS(lock)}}>Skicka</button>
-
-          <button onClick={props.closeModal}>Avbryt</button>
+          <div className="input-row">
+            <label htmlFor="lock">Lås:</label>
+            <select name="lock" id="lock" onChange={(e) => { setLock(e.target.value) }}>
+              <option value="375">Inget</option>
+              <option value="059">Gul</option>
+              <option value="024">Silver</option>
+              <option value="692">Svart</option>
+            </select>
+          </div>
+          <div className="buttons">
+            <button className='secondary-button' onClick={props.closeModal}>Avbryt</button>
+            <button className='primary-button' onClick={() => { props.sendDoneSMS(lock) }}>Skicka</button>
+          </div>
         </div>
       </div>
     </div>
